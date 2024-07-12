23b44a054913f4420a5a27beba6937f2d71a46dc
Dimitris Palamidas <palamidas.dim@gmail.com>
2024-07-12 11:26:29 +0300

    Import motion animation

--- app/components/AboutUs.tsx
+++ app/components/AboutUs.tsx
@@ -1,74 +1,95 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 const AboutUs = () => {
+  const [ref1, inView1] = useInView({ triggerOnce: true, threshold: 0.1 });
+  const [ref2, inView2] = useInView({ triggerOnce: true, threshold: 0.1 });
+  const [ref3, inView3] = useInView({ triggerOnce: true, threshold: 0.1 });
+
   return (
     <section className='container mx-auto my-8 p-4 bg-white rounded-lg shadow-lg'>
-      <h2 className='text-3xl font-bold mb-4 text-blue-900'>Σχετικά με Εμάς</h2>
-      <p className='mb-4 text-gray-700'>
-        Καλώς ήρθατε στην <strong>My-Promotion-Company</strong>, την κορυφαία
-        εταιρεία προώθησης που αφοσιώνεται στο να δίνει ζωή και φωνή στο brand
-        σας. Η αποστολή μας είναι να δημιουργούμε δυναμικές και αξέχαστες
-        εμπειρίες που συνδέουν το κοινό σας με το προϊόν ή την υπηρεσία σας.
-      </p>
-      <h3 className='text-2xl font-semibold mb-2 text-blue-800'>
-        Ποιοι Είμαστε
-      </h3>
-      <p className='mb-4 text-gray-700'>
-        Η <strong>My-Promotion-Company</strong> ιδρύθηκε με ένα απλό αλλά ισχυρό
-        όραμα: να προσφέρουμε καινοτόμες λύσεις προώθησης που υπερβαίνουν τις
-        προσδοκίες των πελατών μας. Με μια ομάδα έμπειρων επαγγελματιών και
-        ειδικών στον τομέα της διαφήμισης και του μάρκετινγκ, έχουμε την
-        τεχνογνωσία και το πάθος να προωθούμε το brand σας με τον πιο αποδοτικό
-        και δημιουργικό τρόπο.
-      </p>
-      <h3 className='text-2xl font-semibold mb-2 text-blue-800'>
-        Οι Υπηρεσίες Μας
-      </h3>
-      <ul className='list-disc list-inside mb-4 text-gray-700'>
-        <li className='mb-2'>
-          <strong>Δημιουργία Εκδηλώσεων:</strong> Οργανώνουμε εκδηλώσεις που
-          ενισχύουν την αναγνωρισιμότητα του brand σας και δημιουργούν ισχυρούς
-          δεσμούς με το κοινό σας.
-        </li>
-        <li className='mb-2'>
-          <strong>Προωθητικές Ενέργειες:</strong> Από δειγματοδιανομές μέχρι
-          καμπάνιες στα social media, σχεδιάζουμε στρατηγικές προώθησης που
-          προσελκύουν και διατηρούν το ενδιαφέρον των πελατών σας.
-        </li>
-        <li className='mb-2'>
-          <strong>Brand Activation:</strong> Ενεργοποιούμε το brand σας με
-          πρωτότυπες και διαδραστικές ενέργειες που ενισχύουν την ταυτότητά σας
-          και δημιουργούν αξέχαστες εμπειρίες.
-        </li>
-        <li className='mb-2'>
-          <strong>Στρατηγικό Μάρκετινγκ:</strong> Αναλύουμε την αγορά και το
-          κοινό σας, και δημιουργούμε προσαρμοσμένες στρατηγικές μάρκετινγκ που
-          οδηγούν σε μετρήσιμα αποτελέσματα.
-        </li>
-      </ul>
-      <h3 className='text-2xl font-semibold mb-2 text-blue-800'>
-        Γιατί Να Μας Επιλέξετε
-      </h3>
-      <ul className='list-disc list-inside mb-4 text-gray-700'>
-        <li className='mb-2'>
-          <strong>Εξειδίκευση και Εμπειρία:</strong> Η ομάδα μας αποτελείται από
-          επαγγελματίες με χρόνια εμπειρίας και εξειδίκευσης στον τομέα της
-          προώθησης.
-        </li>
-        <li className='mb-2'>
-          <strong>Καινοτομία:</strong> Αναζητούμε συνεχώς νέους και
-          δημιουργικούς τρόπους για να προωθούμε τα προϊόντα και τις υπηρεσίες
-          σας.
-        </li>
-        <li className='mb-2'>
-          <strong>Εξατομικευμένη Προσέγγιση:</strong> Κατανοούμε ότι κάθε brand
-          είναι μοναδικό και προσαρμόζουμε τις υπηρεσίες μας για να καλύψουμε
-          τις συγκεκριμένες ανάγκες και στόχους σας.
-        </li>
-        <li className='mb-2'>
-          <strong>Αποτελεσματικότητα:</strong> Δεσμευόμαστε να παρέχουμε υψηλής
-          ποιότητας υπηρεσίες που αποδίδουν πραγματικά αποτελέσματα για το brand
-          σας.
-        </li>
-      </ul>
+      <motion.div
+        ref={ref1}
+        initial={{ opacity: 0, x: -100 }}
+        animate={inView1 ? { opacity: 1, x: 0 } : { opacity: 0, x: -100 }}
+        transition={{ duration: 1, delay: 0.75 }}
+      >
+        <h2 className='text-3xl font-bold mb-4 text-blue-900'>
+          Σχετικά με Εμάς
+        </h2>
+        <p className='mb-4 text-gray-700'>
+          Καλώς ήρθατε στην <strong>My-Promotion-Company</strong>, την κορυφαία
+          εταιρεία προώθησης που αφοσιώνεται στο να δίνει ζωή και φωνή στο brand
+          σας. Η αποστολή μας είναι να δημιουργούμε δυναμικές και αξέχαστες
+          εμπειρίες που συνδέουν το κοινό σας με το προϊόν ή την υπηρεσία σας.
+        </p>
+      </motion.div>
+      <motion.div
+        ref={ref2}
+        initial={{ opacity: 0, x: 100 }}
+        animate={inView2 ? { opacity: 1, x: 0 } : { opacity: 0, x: 100 }}
+        transition={{ duration: 1, delay: 0.75 }}
+      >
+        <h3 className='text-2xl font-semibold mb-2 text-blue-800'>
+          Οι Υπηρεσίες Μας
+        </h3>
+        <ul className='list-disc list-inside mb-4 text-gray-700'>
+          <li className='mb-2'>
+            <strong>Δημιουργία Εκδηλώσεων:</strong> Οργανώνουμε εκδηλώσεις που
+            ενισχύουν την αναγνωρισιμότητα του brand σας και δημιουργούν
+            ισχυρούς δεσμούς με το κοινό σας.
+          </li>
+          <li className='mb-2'>
+            <strong>Προωθητικές Ενέργειες:</strong> Από δειγματοδιανομές μέχρι
+            καμπάνιες στα social media, σχεδιάζουμε στρατηγικές προώθησης που
+            προσελκύουν και διατηρούν το ενδιαφέρον των πελατών σας.
+          </li>
+          <li className='mb-2'>
+            <strong>Brand Activation:</strong> Ενεργοποιούμε το brand σας με
+            πρωτότυπες και διαδραστικές ενέργειες που ενισχύουν την ταυτότητά
+            σας και δημιουργούν αξέχαστες εμπειρίες.
+          </li>
+          <li className='mb-2'>
+            <strong>Στρατηγικό Μάρκετινγκ:</strong> Αναλύουμε την αγορά και το
+            κοινό σας, και δημιουργούμε προσαρμοσμένες στρατηγικές μάρκετινγκ
+            που οδηγούν σε μετρήσιμα αποτελέσματα.
+          </li>
+        </ul>
+      </motion.div>
+      <motion.div
+        ref={ref3}
+        initial={{ opacity: 0, x: -100 }}
+        animate={inView3 ? { opacity: 1, x: 0 } : { opacity: 0, x: -100 }}
+        transition={{ duration: 1, delay: 0.75 }}
+      >
+        <h3 className='text-2xl font-semibold mb-2 text-blue-800'>
+          Γιατί Να Μας Επιλέξετε
+        </h3>
+        <ul className='list-disc list-inside mb-4 text-gray-700'>
+          <li className='mb-2'>
+            <strong>Εξειδίκευση και Εμπειρία:</strong> Η ομάδα μας αποτελείται
+            από επαγγελματίες με χρόνια εμπειρίας και εξειδίκευσης στον τομέα
+            της προώθησης.
+          </li>
+          <li className='mb-2'>
+            <strong>Καινοτομία:</strong> Αναζητούμε συνεχώς νέους και
+            δημιουργικούς τρόπους για να προωθούμε τα προϊόντα και τις υπηρεσίες
+            σας.
+          </li>
+          <li className='mb-2'>
+            <strong>Εξατομικευμένη Προσέγγιση:</strong> Κατανοούμε ότι κάθε
+            brand είναι μοναδικό και προσαρμόζουμε τις υπηρεσίες μας για να
+            καλύψουμε τις συγκεκριμένες ανάγκες και στόχους σας.
+          </li>
+          <li className='mb-2'>
+            <strong>Αποτελεσματικότητα:</strong> Δεσμευόμαστε να παρέχουμε
+            υψηλής ποιότητας υπηρεσίες που αποδίδουν πραγματικά αποτελέσματα για
+            το brand σας.
+          </li>
+        </ul>
+      </motion.div>
     </section>
   );
 };
--- app/components/Testimonials.tsx
+++ app/components/Testimonials.tsx
@@ -1,27 +1,14 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import Image from "next/image";
 
-const testimonials = [
-  {
-    text: "Θα τους επέλεγα ξανά και ξανά!",
-    name: "Dimitris",
-    description: "Authorized Coca-cola reseller",
-    avatar: "/user.png",
-  },
-  {
-    text: "Άψογη συνεργασία, με σύνεση στον στόχο μας.",
-    name: "Achilleas",
-    description: "Founder of 'ΑΦΟΙ ΛΑΜΠΟΥ'",
-    avatar: "/user.png",
-  },
-  {
-    text: "Σας ευχαριστώ για όλα!",
-    name: "Pavlina",
-    description: "Project Manager",
-    avatar: "/user.png",
-  },
-];
-
 const Testimonials = () => {
+  const [ref1, inView1] = useInView({ triggerOnce: true, threshold: 0.1 });
+  const [ref2, inView2] = useInView({ triggerOnce: true, threshold: 0.1 });
+  const [ref3, inView3] = useInView({ triggerOnce: true, threshold: 0.1 });
+
   return (
     <section className='py-12 px-4'>
       <div className='container mx-auto'>
@@ -32,28 +19,88 @@ const Testimonials = () => {
           Δείτε τι έγραψαν οι πελάτες μας για εμάς.
         </p>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
-          {testimonials.map((testimonial, index) => (
-            <div key={index} className='bg-white p-6 rounded-lg shadow-lg'>
-              <p className='text-gray-800 mb-4'>“{testimonial.text}”</p>
-              <div className='flex items-center'>
-                <div className='w-10 h-10 rounded-full overflow-hidden'>
-                  <Image
-                    src={testimonial.avatar}
-                    alt='Avatar'
-                    width={40}
-                    height={40}
-                    objectFit='cover'
-                  />
-                </div>
-                <div className='ml-4'>
-                  <p className='font-bold text-blue-900'>{testimonial.name}</p>
-                  <p className='text-gray-600 text-sm'>
-                    {testimonial.description}
-                  </p>
-                </div>
+          <motion.div
+            ref={ref1}
+            initial={{ opacity: 0, x: -100 }}
+            animate={inView1 ? { opacity: 1, x: 0 } : { opacity: 0, x: -100 }}
+            transition={{ duration: 1, delay: 0.2 }}
+            className='bg-white p-6 rounded-lg shadow-lg'
+          >
+            <p className='text-gray-800 mb-4'>
+              “Θα τους επέλεγα ξανά και ξανά!”
+            </p>
+            <div className='flex items-center'>
+              <div className='w-10 h-10 rounded-full overflow-hidden'>
+                <Image
+                  src='/user.png'
+                  alt='Avatar'
+                  width={40}
+                  height={40}
+                  objectFit='cover'
+                />
+              </div>
+              <div className='ml-4'>
+                <p className='font-bold text-blue-900'>Dimitris</p>
+                <p className='text-gray-600 text-sm'>
+                  Authorized Coca-cola reseller
+                </p>
               </div>
             </div>
-          ))}
+          </motion.div>
+
+          <motion.div
+            ref={ref2}
+            initial={{ opacity: 0, x: -100 }}
+            animate={inView2 ? { opacity: 1, x: 0 } : { opacity: 0, x: -100 }}
+            transition={{ duration: 1, delay: 0.2 }}
+            className='bg-white p-6 rounded-lg shadow-lg'
+          >
+            <p className='text-gray-800 mb-4'>
+              “Άψογη συνεργασία, με σύνεση στον στόχο μας.”
+            </p>
+            <div className='flex items-center'>
+              <div className='w-10 h-10 rounded-full overflow-hidden'>
+                <Image
+                  src='/user.png'
+                  alt='Avatar'
+                  width={40}
+                  height={40}
+                  objectFit='cover'
+                />
+              </div>
+              <div className='ml-4'>
+                <p className='font-bold text-blue-900'>Achilleas</p>
+                <p className='text-gray-600 text-sm'>
+                  Founder of &apos;ΑΦΟΙ ΛΑΜΠΟΥ&apos;
+                </p>
+              </div>
+            </div>
+          </motion.div>
+
+          <motion.div
+            ref={ref3}
+            initial={{ opacity: 0, x: -100 }}
+            animate={inView3 ? { opacity: 1, x: 0 } : { opacity: 0, x: -100 }}
+            transition={{ duration: 1, delay: 0.4 }}
+            className='bg-white p-6 rounded-lg shadow-lg'
+          >
+            <p className='text-gray-800 mb-4'>“Σας ευχαριστώ για όλα!”</p>
+            <div className='flex items-center'>
+              <div className='w-10 h-10 rounded-full overflow-hidden'>
+                <Image
+                  src='/user.png'
+                  alt='Avatar'
+                  width={40}
+                  height={40}
+                  objectFit='cover'
+                />
+              </div>
+              <div className='ml-4'>
+                <p className='font-bold text-blue-900'>Pavlina</p>
+                <p className='text-gray-600 text-sm'>Project Manager</p>
+              </div>
+            </div>
+          </motion.div>
         </div>
       </div>
     </section>
