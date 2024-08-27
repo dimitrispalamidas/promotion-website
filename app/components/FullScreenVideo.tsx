b39a0058f86ac89ef49e12d6d4a78632c87084c0
Dimitris Palamidas <palamidas.dim@gmail.com>
2024-08-27 22:47:22 +0300

    fix bug for video on mobile

--- app/components/FullScreenVideo.tsx
+++ app/components/FullScreenVideo.tsx
@@ -15,11 +15,15 @@ const FullscreenVideo = () => {
       <div className='relative w-full h-screen overflow-hidden -mt-[82px]'>
         <video
           className='absolute inset-0 w-full h-full object-cover'
-          src='/promotion-video.mp4'
           autoPlay
           loop
           muted
-        />
+          playsInline
+        >
+          <source src='/promotion-video.mp4' type='video/mp4' />
+          <source src='/promotion-video.webm' type='video/webm' />
+          Your browser does not support the video tag.
+        </video>
         <div className='absolute inset-0 flex flex-col items-center justify-center px-4'>
           <h1
             className={`text-white text-4xl font-bold leading-loose text-center ${nunito.className}`}
